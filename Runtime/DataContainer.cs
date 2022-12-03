38e0c1c670772cfdc94a06a1b50ed24be6bfe42d
Victor Bisterfeld <gbvictor@mail.ru>
2022-12-03 01:52:51 +0100

    add usages

--- Runtime/DataContainer.cs
+++ Runtime/DataContainer.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace com.gbviktor.DataContainer
 {
     public class DataContainer : IDisposable
--- Runtime/DataContainerExtensions.cs
+++ Runtime/DataContainerExtensions.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace com.gbviktor.DataContainer
 {
     public static class DataContainerExtensions
